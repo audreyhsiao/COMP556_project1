1585122d1bd6abdbb3f2118c79acf114a42be0dc
audreyhsiao <audreyyj13@gmail.com>
2024-09-10 14:49:06 -0500

    Update server_num.c

--- server_num.c
+++ server_num.c
@@ -1,18 +1,17 @@
+#include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
 
 /**************************************************/
 /* a few simple linked list functions             */
 /**************************************************/
-
 
 /* A linked list node data structure to maintain application
    information related to a connected socket */
@@ -58,21 +57,20 @@ void add(struct node *head, int socket, struct sockaddr_in addr) {
   head->next = new_node;
 }
 
-
 /*****************************************/
 /* main program                          */
 /*****************************************/
 
 /* simple server, takes one parameter, the server port number */
 int main(int argc, char **argv) {
-
   /* socket and option variables */
   int sock, new_sock, max;
   int optval = 1;
 
   /* server socket address variables */
   struct sockaddr_in sin, addr;
-  // The port on which the server should run (on CLEAR, the usable range is 18000 <= port <= 18200).
+  // The port on which the server should run (on CLEAR, the usable range is
+  // 18000 <= port <= 18200).
   unsigned short server_port = atoi(argv[1]);
 
   /* socket address variables for a connected client */
@@ -110,228 +108,221 @@ int main(int argc, char **argv) {
   head.next = 0;
 
   /* create a server socket to listen for TCP connection requests */
-  if ((sock = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-    {
-      perror ("opening TCP socket");
-      abort ();
-    }
-  
+  if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    perror("opening TCP socket");
+    abort();
+  }
+
   /* set option so we can reuse the port number quickly after a restart */
-  if (setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof (optval)) <0)
-    {
-      perror ("setting TCP socket option");
-      abort ();
-    }
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+    perror("setting TCP socket option");
+    abort();
+  }
 
   /* fill in the address of the server socket */
-  memset (&sin, 0, sizeof (sin));
+  memset(&sin, 0, sizeof(sin));
   sin.sin_family = AF_INET;
   sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_port = htons (server_port);
-  
+  sin.sin_port = htons(server_port);
+
   /* bind server socket to the address */
-  if (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0)
-    {
-      perror("binding socket to address");
-      abort();
-    }
+  if (bind(sock, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    perror("binding socket to address");
+    abort();
+  }
 
   /* put the server socket in listen mode */
-  if (listen (sock, BACKLOG) < 0)
-    {
-      perror ("listen on socket failed");
-      abort();
-    }
+  if (listen(sock, BACKLOG) < 0) {
+    perror("listen on socket failed");
+    abort();
+  }
 
   /* now we keep waiting for incoming connections,
      check for incoming data to receive,
      check for ready socket to send more data */
-  while (1)
-    {
+  while (1) {
+    /* set up the file descriptor bit map that select should be watching */
+    FD_ZERO(&read_set);  /* clear everything */
+    FD_ZERO(&write_set); /* clear everything */
 
-      /* set up the file descriptor bit map that select should be watching */
-      FD_ZERO (&read_set); /* clear everything */
-      FD_ZERO (&write_set); /* clear everything */
+    FD_SET(sock, &read_set); /* put the listening socket in */
+    max = sock;              /* initialize max */
 
-      FD_SET (sock, &read_set); /* put the listening socket in */
-      max = sock; /* initialize max */
+    /* put connected sockets into the read and write sets to monitor them */
+    // Run through the Linked list of socket
+    for (current = head.next; current; current = current->next) {
+      FD_SET(current->socket, &read_set);
 
-      /* put connected sockets into the read and write sets to monitor them */
-      for (current = head.next; current; current = current->next) {
-	FD_SET(current->socket, &read_set);
-
-	if (current->pending_data) {
-	  /* there is data pending to be sent, monitor the socket
-             in the write set so we know when it is ready to take more
-             data */
-	  FD_SET(current->socket, &write_set);
-	}
-
-	if (current->socket > max) {
-	  /* update max if necessary */
-	  max = current->socket;
-	}
+      if (current->pending_data) {
+        /* there is data pending to be sent, monitor the socket
+           in the write set so we know when it is ready to take more
+           data */
+        FD_SET(current->socket, &write_set);
       }
 
-      time_out.tv_usec = 100000; /* 1-tenth of a second timeout */
-      time_out.tv_sec = 0;
-
-      /* invoke select, make sure to pass max+1 !!! */
-      select_retval = select(max+1, &read_set, &write_set, NULL, &time_out);
-      if (select_retval < 0)
-	{
-	  perror ("select failed");
-	  abort ();
-	}
-
-      if (select_retval == 0)
-	{
-	  /* no descriptor ready, timeout happened */
-	  continue;
-	}
-      
-      if (select_retval > 0) /* at least one file descriptor is ready */
-	{
-	  if (FD_ISSET(sock, &read_set)) /* check the server socket */
-	    {
-	      /* there is an incoming connection, try to accept it */
-	      new_sock = accept (sock, (struct sockaddr *) &addr, &addr_len);
-	      
-	      if (new_sock < 0)
-		{
-		  perror ("error accepting connection");
-		  abort ();
-		}
-
-	      /* make the socket non-blocking so send and recv will
-                 return immediately if the socket is not ready.
-                 this is important to ensure the server does not get
-                 stuck when trying to send data to a socket that
-                 has too much data to send already.
-               */
-	      if (fcntl (new_sock, F_SETFL, O_NONBLOCK) < 0)
-		{
-		  perror ("making socket non-blocking");
-		  abort ();
-		}
-  
-	      /* the connection is made, everything is ready */
-	      /* let's see who's connecting to us */
-	      printf("Accepted connection. Client IP address is: %s\n",
-		     inet_ntoa(addr.sin_addr));
-
-	      /* remember this client connection in our linked list */
-	      add(&head, new_sock, addr);
-
-	      /* let's send a message to the client just for fun */
-	      count = send(new_sock, message, strlen(message)+1, 0);
-	      if (count < 0)
-		{
-		  perror("error sending message to client");
-		  abort();
-		}
-	    }
-
-	  /* check other connected sockets, see if there is
-             anything to read or some socket is ready to send
-             more pending data */
-	  for (current = head.next; current; current = next) {
-	    next = current->next;
-
-	    /* see if we can now do some previously unsuccessful writes */
-	    if (FD_ISSET(current->socket, &write_set)) {
-	      /* the socket is now ready to take more data */
-	      /* the socket data structure should have information
-                 describing what data is supposed to be sent next.
-	         but here for simplicity, let's say we are just
-                 sending whatever is in the buffer buf
-               */
-	      count = send(current->socket, buf, BUF_LEN, MSG_DONTWAIT);
-	      if (count < 0) {
-		if (errno == EAGAIN) {
-		  /* we are trying to dump too much data down the socket,
-		     it cannot take more for the time being 
-		     will have to go back to select and wait til select
-		     tells us the socket is ready for writing
-		  */
-		} else {
-		  /* something else is wrong */
-		}
-	      }
-	      /* note that it is important to check count for exactly
-                 how many bytes were actually sent even when there are
-                 no error. send() may send only a portion of the buffer
-                 to be sent.
-	      */
-	    }
-
-	    if (FD_ISSET(current->socket, &read_set)) {
-	      /* we have data from a client */
-	      
-	      count = recv(current->socket, buf, BUF_LEN, 0);
-	      if (count <= 0) {
-		/* something is wrong */
-		if (count == 0) {
-		  printf("Client closed connection. Client IP address is: %s\n", inet_ntoa(current->client_addr.sin_addr));
-		} else {
-		  perror("error receiving from a client");
-		}
-
-		/* connection is closed, clean up */
-		close(current->socket);
-		dump(&head, current->socket);
-	      } else {
-		/* we got count bytes of data from the client */
-                /* in general, the amount of data received in a recv()
-                   call may not be a complete application message. it
-                   is important to check the data received against
-                   the message format you expect. if only a part of a
-                   message has been received, you must wait and
-                   receive the rest later when more data is available
-                   to be read */
-		/* in this case, we expect a message where the first byte
-                   stores the number of bytes used to encode a number, 
-                   followed by that many bytes holding a numeric value */
-		if (buf[0]+1 != count) {
-                  /* we got only a part of a message, we won't handle this in
-                     this simple example */
-		  printf("Message incomplete, something is still being transmitted\n");
-		  return 0;
-		} else {
-		  switch(buf[0]) {
-		  case 1:
-		    /* note the type casting here forces signed extension
-		       to preserve the signedness of the value */
-		    /* note also the use of parentheses for pointer 
-		       dereferencing is critical here */
-		    num = (char) *(char *)(buf+1);
-		    break;
-		  case 2:
-		    /* note the type casting here forces signed extension
-		       to preserve the signedness of the value */
-		    /* note also the use of parentheses for pointer 
-		       dereferencing is critical here */
-		    /* note for 16 bit integers, byte ordering matters */
-		    num = (short) ntohs(*(short *)(buf+1));
-		    break;
-		  case 4:
-		    /* note the type casting here forces signed extension
-		       to preserve the signedness of the value */
-		    /* note also the use of parentheses for pointer 
-		       dereferencing is critical here */
-		    /* note for 32 bit integers, byte ordering matters */
-		    num = (int) ntohl(*(int *)(buf+1));
-		    break;
-		  default:
-		    break;
-		  }
-                  /* a complete message is received, print it out */
-		  printf("Received the number \"%d\". Client IP address is: %s\n",
-			 num, inet_ntoa(current->client_addr.sin_addr));
-		}
-	      }
-	    }
-	  }
-	}
+      if (current->socket > max) {
+        /* update max if necessary */
+        max = current->socket;
+      }
     }
+
+    time_out.tv_usec = 100000; /* 1-tenth of a second timeout */
+    time_out.tv_sec = 0;
+
+    /* invoke select, make sure to pass max+1 !!! */
+    select_retval = select(max + 1, &read_set, &write_set, NULL, &time_out);
+    if (select_retval < 0) {
+      perror("select failed");
+      abort();
+    }
+    // TODO what to do with it when Timeout
+    if (select_retval == 0) {
+      /* no descriptor ready, timeout happened */
+      continue;
+    }
+
+    if (select_retval > 0) /* at least one file descriptor is ready */
+    {
+      if (FD_ISSET(sock, &read_set)) /* check the server socket */ // this is the listening server that is still there
+      {
+        /* there is an incoming connection, try to accept it */
+        new_sock = accept(sock, (struct sockaddr *)&addr, &addr_len);
+
+        if (new_sock < 0) {
+          perror("error accepting connection");
+          abort();
+        }
+
+        /* make the socket non-blocking so send and recv will
+           return immediately if the socket is not ready.
+           this is important to ensure the server does not get
+           stuck when trying to send data to a socket that
+           has too much data to send already.
+         */
+        if (fcntl(new_sock, F_SETFL, O_NONBLOCK) < 0) {
+          perror("making socket non-blocking");
+          abort();
+        }
+
+        /* the connection is made, everything is ready */
+        /* let's see who's connecting to us */
+        printf("Accepted connection. Client IP address is: %s\n",
+               inet_ntoa(addr.sin_addr));
+
+        /* remember this client connection in our linked list */
+        add(&head, new_sock, addr);
+
+        /* let's send a message to the client just for fun */
+        count = send(new_sock, message, strlen(message) + 1, 0);
+        if (count < 0) {
+          perror("error sending message to client");
+          abort();
+        }
+      }
+
+      /* check other connected sockets, see if there is
+         anything to read or some socket is ready to send
+         more pending data */
+      for (current = head.next; current; current = next) {
+        next = current->next;
+
+        /* see if we can now do some previously unsuccessful writes */
+        if (FD_ISSET(current->socket, &write_set)) {
+          /* the socket is now ready to take more data */
+          /* the socket data structure should have information
+             describing what data is supposed to be sent next.
+             but here for simplicity, let's say we are just
+             sending whatever is in the buffer buf
+           */
+          count = send(current->socket, buf, BUF_LEN, MSG_DONTWAIT);
+          if (count < 0) {
+            if (errno == EAGAIN) {
+              /* we are trying to dump too much data down the socket,
+                 it cannot take more for the time being
+                 will have to go back to select and wait til select
+                 tells us the socket is ready for writing
+              */
+            } else {
+              /* something else is wrong */
+            }
+          }
+          /* note that it is important to check count for exactly
+             how many bytes were actually sent even when there are
+             no error. send() may send only a portion of the buffer
+             to be sent.
+          */
+        }
+
+        if (FD_ISSET(current->socket, &read_set)) {
+          /* we have data from a client */
+
+          count = recv(current->socket, buf, BUF_LEN, 0);
+          if (count <= 0) {
+            /* something is wrong */
+            if (count == 0) {
+              printf("Client closed connection. Client IP address is: %s\n",
+                     inet_ntoa(current->client_addr.sin_addr));
+            } else {
+              perror("error receiving from a client");
+            }
+
+            /* connection is closed, clean up */
+            close(current->socket);
+            dump(&head, current->socket);
+          } else {
+            /* we got count bytes of data from the client */
+            /* in general, the amount of data received in a recv()
+               call may not be a complete application message. it
+               is important to check the data received against
+               the message format you expect. if only a part of a
+               message has been received, you must wait and
+               receive the rest later when more data is available
+               to be read */
+            /* in this case, we expect a message where the first byte
+               stores the number of bytes used to encode a number,
+               followed by that many bytes holding a numeric value */
+            if (buf[0] + 1 != count) {
+              /* we got only a part of a message, we won't handle this in
+                 this simple example */
+              printf(
+                  "Message incomplete, something is still being transmitted\n");
+              return 0;
+            } else {
+              switch (buf[0]) {
+                case 1:
+                  /* note the type casting here forces signed extension
+                     to preserve the signedness of the value */
+                  /* note also the use of parentheses for pointer
+                     dereferencing is critical here */
+                  num = (char)*(char *)(buf + 1);
+                  break;
+                case 2:
+                  /* note the type casting here forces signed extension
+                     to preserve the signedness of the value */
+                  /* note also the use of parentheses for pointer
+                     dereferencing is critical here */
+                  /* note for 16 bit integers, byte ordering matters */
+                  num = (short)ntohs(*(short *)(buf + 1));
+                  break;
+                case 4:
+                  /* note the type casting here forces signed extension
+                     to preserve the signedness of the value */
+                  /* note also the use of parentheses for pointer
+                     dereferencing is critical here */
+                  /* note for 32 bit integers, byte ordering matters */
+                  num = (int)ntohl(*(int *)(buf + 1));
+                  break;
+                default:
+                  break;
+              }
+              /* a complete message is received, print it out */
+              printf("Received the number \"%d\". Client IP address is: %s\n",
+                     num, inet_ntoa(current->client_addr.sin_addr));
+              //TODO sent pong message: sent same massage back to the client(ip address: current->client_addr.sin_addr)
+            }
+          }
+        }
+      }
+    }
+  }
 }
